635b1d9f1df9e5caafe31aa986be5cff1a0a6b4a
Sumana Panda <143005457+sumanapanda@users.noreply.github.com>
2025-04-11 20:10:59 +0530

    final medicSystem

---
+++ src/java/logoutServlet.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class logoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        res.sendRedirect("homePage.html");
+    }
+}
